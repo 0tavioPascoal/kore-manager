5f0e4b2fa298e39587c0739aabc59f3a35a348b8
0tavioPascoal <fjoooosa@gmail.com>
2026-04-12 23:19:54 -0300

    feat: add sidebar and sideBar button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
-    <h1 className="ml-6 mt-6 text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Sidebar />;
+    </div>
   );
 }
 
